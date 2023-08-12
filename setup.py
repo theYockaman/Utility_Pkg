d56fc829664f39e609cc1182b5c17160d6123cdf
Nathan Yockey <nathanyock@icloud.com>
2023-08-11 21:15:03 -0500

    Setup.py Change

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = "utils"
+    , version = "0.0.1"
+    , author = "Nathan Yockey"
+    , description = "Utils Package is universal across building of other Python Projects"
+    , packages = find_packages()
+)
