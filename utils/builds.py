381e606242c9146ee34ce346e293ed4c0bd8813d
Nathan Yockey <nathanyock@icloud.com>
2023-12-18 18:49:13 -0600

    Directory Changes for Testing

--- utils/builds.py
+++ utils/builds.py
@@ -23,8 +23,9 @@ def createWebsite(php:bool = False) -> None:
     
 
     # Index Page Creation
-    print(f'{PARENT_DIR}/templates/{bType.upper()}/index.{bType}')
-    File(f'{PARENT_DIR}/templates/{bType.upper()}/index.{bType}',bType, False).copy(f'index.{bType}')
+    print(f'templates/{bType.upper()}/index.{bType}')
+    print(PARENT_DIR)
+    File(f'templates/{bType.upper()}/index.{bType}',bType, False).copy(f'index.{bType}')
     
     # Base File Type, CSS, JS Folders
     bFolder = Folder(bType.upper())
@@ -32,20 +33,20 @@ def createWebsite(php:bool = False) -> None:
     Folder('Content')
     
     # Add Files to Folders
-    File(f"{PARENT_DIR}/templates/JS/main.js",'js',False).copy(f"{jsFolder.directory}/main.js")
+    File(f"templates/JS/main.js",'js',False).copy(f"{jsFolder.directory}/main.js")
     
     # JQuery File
-    File(f'{PARENT_DIR}/templates/JS/jquery-3.7.1.js','js',False).copy(f'{jsFolder.directory}/jquery.js')
+    File(f'templates/JS/jquery-3.7.1.js','js',False).copy(f'{jsFolder.directory}/jquery.js')
     
     # Reset CSS File
-    Folder(f'{PARENT_DIR}/templates/CSS').copy(f'CSS')
+    Folder(f'templates/CSS').copy(f'CSS')
     
     # Add HTML/PHP File
-    File(f'{PARENT_DIR}/templates/HTML/other.html','html').copy(f"{bType.upper()}/other.{bType}")
+    File(f'templates/HTML/other.html','html').copy(f"{bType.upper()}/other.{bType}")
     
     # PHP Files
     if bType == 'php': 
-        File(f'{PARENT_DIR}/templates/PHP/init.php','php',False).copy(f"{bFolder.directory}/init.php")
+        File(f'templates/PHP/init.php','php',False).copy(f"{bFolder.directory}/init.php")
     
     
     
