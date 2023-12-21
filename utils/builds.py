4665b9908eefb60c99b9bdc1dd92958d34cad248
Nathan Yockey <nathanyock@icloud.com>
2023-12-20 22:30:26 -0600

    Update the Build with better directories

--- utils/builds.py
+++ utils/builds.py
@@ -29,20 +29,20 @@ def createWebsite(php:bool = False) -> None:
     
     # Add Files to Folders
     
-    File(f"./templates/JS/main.js",'js',False).copy(f"{jsFolder.directory}/main.js")
+    File(f"utils/templates/JS/main.js",'js',False).copy(f"{jsFolder.directory}/main.js")
     
     # JQuery File
-    File(f'./templates/JS/jquery-3.7.1.js','js',False).copy(f'{jsFolder.directory}/jquery.js')
+    File(f'utils/templates/JS/jquery-3.7.1.js','js',False).copy(f'{jsFolder.directory}/jquery.js')
     
     # Reset CSS File
-    Folder(f'./templates/CSS').copy(f'CSS')
+    Folder(f'utils/templates/CSS').copy(f'CSS')
     
     # Add HTML/PHP File
-    File(f'./templates/HTML/other.html','html').copy(f"{bType.upper()}/other.{bType}")
+    File(f'utils/templates/HTML/other.html','html').copy(f"{bType.upper()}/other.{bType}")
     
     # PHP Files
     if bType == 'php': 
-        File(f'./templates/PHP/init.php','php',False).copy(f"{bFolder.directory}/init.php")
+        File(f'utils/templates/PHP/init.php','php',False).copy(f"{bFolder.directory}/init.php")
     
     
     
